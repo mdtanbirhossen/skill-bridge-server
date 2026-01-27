725c798bad78ac98c8b400d8ef10a667bce0d008
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2026-01-27 18:24:47 +0600

    feat: completed login and register user module

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,80 @@
+import { Request, Response } from "express";
+import { AuthService } from "./auth.service";
+
+const register = async (req: Request, res: Response) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Name, email and password are required",
+      });
+    }
+
+    const result = await AuthService.createUser({
+      name,
+      email,
+      password,
+      role,
+    });
+
+    res.cookie("token", result.token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      data: result,
+    });
+  } catch (error: any) {
+    return res.status(400).json({
+      success: false,
+      message: error.message || "Registration failed",
+    });
+  }
+};
+
+const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and password are required",
+      });
+    }
+
+    const result = await AuthService.signInUser({
+      email,
+      password,
+    });
+
+    res.cookie("token", result.token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    return res.status(200).json({
+      success: true,
+      message: "Login successful",
+      data: result,
+    });
+  } catch (error: any) {
+    return res.status(401).json({
+      success: false,
+      message: error.message || "Invalid email or password",
+    });
+  }
+};
+
+export const AuthController = {
+  register,
+  login,
+};
---
+++ src/modules/auth/auth.routes.ts
@@ -0,0 +1,9 @@
+import express, { NextFunction, Request, Response } from "express";
+import { AuthController } from "./auth.controller";
+
+const router = express.Router();
+
+router.post("/register", AuthController.register);
+router.post("/login", AuthController.login);
+
+export const AuthRoutes = router;
