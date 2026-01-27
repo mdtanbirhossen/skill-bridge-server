6241d365dbbdf0d0a5fb85c3b237ca61e7e39c59
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2026-01-27 18:24:16 +0600

    feat: user login and register method created

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,111 @@
+import { prisma } from "../../lib/prisma";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { Role } from "../../../generated/prisma/enums";
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
+const JWT_EXPIRES_IN = "7d";
+
+// create user
+const createUser = async (data: {
+  name: string;
+  email: string;
+  password: string;
+  role?: Role;
+}) => {
+  // check user already exists
+  const existingUser = await prisma.user.findUnique({
+    where: { email: data.email },
+  });
+
+  if (existingUser) {
+    throw new Error("User already exists");
+  }
+
+  // hash password
+  const hashedPassword = await bcrypt.hash(data.password, 12);
+
+  const user = await prisma.user.create({
+    data: {
+      name: data.name,
+      email: data.email,
+      password: hashedPassword,
+      role: data.role ?? Role.STUDENT,
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+    },
+  });
+
+  // generate jwt
+  const token = jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      role: user.role,
+    },
+    JWT_SECRET,
+    { expiresIn: JWT_EXPIRES_IN }
+  );
+
+  return {
+    user,
+    token,
+  };
+};
+
+
+
+// sign in user
+const signInUser = async (data: {
+  email: string;
+  password: string;
+}) => {
+  const user = await prisma.user.findUnique({
+    where: { email: data.email },
+  });
+
+  if (!user) {
+    throw new Error("Invalid email or password");
+  }
+
+  const isPasswordMatch = await bcrypt.compare(
+    data.password,
+    user.password
+  );
+
+  if (!isPasswordMatch) {
+    throw new Error("Invalid email or password");
+  }
+
+  const token = jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      role: user.role,
+    },
+    JWT_SECRET,
+    { expiresIn: JWT_EXPIRES_IN }
+  );
+
+  return {
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    },
+    token,
+  };
+};
+
+
+export const AuthService = {
+  createUser,
+  signInUser,
+};
